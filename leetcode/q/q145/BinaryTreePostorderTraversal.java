bbfea7ddad0ba6e0c3d55a62f6fc26275da80975
Zane Wang <zane.wang@myob.com>
2020-12-20 00:00:59 +1100

    145

---
+++ leetcode/q/q145/BinaryTreePostorderTraversal.java
@@ -0,0 +1,104 @@
+//Given the root of a binary tree, return the postorder traversal of its nodes' 
+//values. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,null,2,3]
+//Output: [3,2,1]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// Example 4: 
+//
+// 
+//Input: root = [1,2]
+//Output: [2,1]
+// 
+//
+// Example 5: 
+//
+// 
+//Input: root = [1,null,2]
+//Output: [2,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of the nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// Follow up: 
+//
+// Recursive solution is trivial, could you do it iteratively? 
+//
+// 
+// Related Topics Stack Tree 
+// 👍 2245 👎 108
+
+
+package q.q145;
+
+import dataStructure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreePostorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        List<Integer> res = new ArrayList<>();
+        public List<Integer> postorderTraversal(TreeNode root) {
+            helper(root);
+            return res;
+        }
+
+
+        void helper(TreeNode node){
+            if (node == null) return;
+            helper(node.left);
+            helper(node.right);
+            res.add(node.val);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
