9d50b307e05e2b12b3336965dd4a739edba4c383
Zane.W <parazane@gmail.com>
2021-08-10 19:01:43 +1000

    day 37

---
+++ leetcode/q/q876/MiddleOfTheLinkedList.java
@@ -0,0 +1,72 @@
+//Given the head of a singly linked list, return the middle node of the linked l
+//ist. 
+//
+// If there are two middle nodes, return the second middle node. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5]
+//Output: [3,4,5]
+//Explanation: The middle node of the list is node 3.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2,3,4,5,6]
+//Output: [4,5,6]
+//Explanation: Since the list has two middle nodes with values 3 and 4, we retur
+//n the second one.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [1, 100]. 
+// 1 <= Node.val <= 100 
+// 
+// Related Topics Linked List Two Pointers 
+// 👍 2914 👎 81
+
+  
+  package q.q876;
+
+import dataStructure.ListNode;
+
+public class MiddleOfTheLinkedList{
+      public static void main(String[] args) {
+           Solution solution = new MiddleOfTheLinkedList().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
