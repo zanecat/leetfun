8838d7535dca988d8a20b879e4268c3943856f89
Zane.W <parazane@gmail.com>
2021-09-04 22:42:31 +1000

    day 59

---
+++ leetcode/q/q135/Candy.java
@@ -0,0 +1,82 @@
+//There are n children standing in a line. Each child is assigned a rating value
+// given in the integer array ratings. 
+//
+// You are giving candies to these children subjected to the following requireme
+//nts: 
+//
+// 
+// Each child must have at least one candy. 
+// Children with a higher rating get more candies than their neighbors. 
+// 
+//
+// Return the minimum number of candies you need to have to distribute the candi
+//es to the children. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: ratings = [1,0,2]
+//Output: 5
+//Explanation: You can allocate to the first, second and third child with 2, 1, 
+//2 candies respectively.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: ratings = [1,2,2]
+//Output: 4
+//Explanation: You can allocate to the first, second and third child with 1, 2, 
+//1 candies respectively.
+//The third child gets 1 candy because it satisfies the above two conditions.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == ratings.length 
+// 1 <= n <= 2 * 104 
+// 0 <= ratings[i] <= 2 * 104 
+// 
+// Related Topics Array Greedy 
+// 👍 2148 👎 224
+
+
+package q.q135;
+
+public class Candy {
+    public static void main(String[] args) {
+        Solution solution = new Candy().new Solution();
+        solution.candy(new int[]{1, 0, 2});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int candy(int[] ratings) {
+            int len = ratings.length;
+            int [] res = new int[len];
+            res[0] = 1;
+            for(int i = 1; i < len; i ++){
+                if (ratings[i] > ratings[i - 1]){
+                    res[i] = res[i - 1] + 1;
+                } else {
+                    res[i] = 1;
+                }
+            }
+
+            int min = res[len - 1];
+            for (int i = len - 2; i >= 0; i -- ){
+                if (ratings[i] > ratings[i + 1]){
+                    res[i] = Math.max(res[i], res[i + 1] + 1);
+                }
+
+                min += res[i];
+            }
+            return min;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
